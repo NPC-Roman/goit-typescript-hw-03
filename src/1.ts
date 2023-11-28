e91244cbcaf681dd4eaacfe932df46c077e829cb
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-11-29 00:26:52 +0200

    change00

--- src/1.ts
+++ src/1.ts
@@ -2,7 +2,7 @@
   Клас Student, який містить три властивості: name, age та grade. 
   Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, 
   напишіть скорочену ініціалізацію.
-*/
+
 
 class Student {
   public name: string;
@@ -17,3 +17,7 @@ class Student {
 }
 
 export {};
+*/
+class Student {
+  constructor(public name: string, public age: number, public grade: string) {}
+}
--- src/2.ts
+++ src/2.ts
@@ -11,9 +11,6 @@
   Клас Manager повинен бути підклас класу Employee
 
   Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
-
-*/
-
 class Employee {
   // Заповніть модифікатори доступу
   name: string;
@@ -35,5 +32,26 @@ class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
-
 export {};
+*/
+class Employee {
+  public name: string;
+  private department: string;
+  protected salary: number;
+
+  constructor(name: string, department: string, salary: number) {
+    this.name = name;
+    this.department = department;
+    this.salary = salary;
+  }
+
+  getEmployeeDetails() {
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
+}
